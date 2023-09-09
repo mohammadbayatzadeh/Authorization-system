f525ac7dc9275d6a215dfaccccc11727c807e5f7
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-09-10 00:18:57 +0330

    Update checkAuth.js

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -3,35 +3,25 @@ const { verifyToken } = require("../utils/auth");
 const { connectDB } = require("../utils/connectDB");
 
 const checkAuth = async (req, res, next) => {
-  const { authorization } = req?.headers ?? false;
+  const { authorization } = req?.headers;
 
-  try{
-    
-  }
-  if (authorization) {
+  try {
     const [bearer, token] = authorization.split(" ");
-    if (bearer && bearer.toLowerCase() === "bearer") {
-      if (token) {
-        const verify = verifyToken(token);
-        if (verify) {
-          await connectDB();
-          const user = await User.findOne({ email: verify.email });
-          req.isAuthenticated = !!user;
-          if (!user)
-            next({ status: 401, message: "not found user login again" });
-          req.user = {
-            fullName: user.firstName + " " + user.lastName,
-            email: user.email,
-          };
-          return next();
-        }
-        next({ status: 401, message: "authentication fialed login again" });
-      }
+    bearer.toLowerCase() !== "bearer" &&
       next({ status: 401, message: "authentication fialed login again" });
-    }
+    const verify = verifyToken(token);
+    await connectDB();
+    const user = await User.findOne({ email: verify.email });
+    req.isAuthenticated = !!user;
+    if (!user) next({ status: 401, message: "not found user login again" });
+    req.user = {
+      fullName: user.firstName + " " + user.lastName,
+      email: user.email,
+    };
+    return next();
+  } catch (err) {
     next({ status: 401, message: "authentication fialed login again" });
   }
-  next({ status: 401, message: "authentication fialed login again" });
 };
 
 module.exports = {
